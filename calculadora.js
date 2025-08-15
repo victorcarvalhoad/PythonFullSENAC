4e59cc5405f3fef4234c6328953298b248cdcccf
victorcarvalhoad <victor.carvalhoad@gmail.com>
2025-08-15 00:11:35 -0300

    atividade js e git

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+let numero1 = 10;
+let numero2 = 2;
+
+/*
+Operadores:
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão
+*/
+operador = 1;
+switch (operador) {
+    case 1:
+        console.log("A soma dos números é:", numero1 + numero2);
+        break;
+    case 2:
+        console.log("A subtração dos nnúmeros é:", numero1 - numero2);
+        break;
+    case 3:
+        console.log("A multiplicação dos nnúmeros é:", numero1 * numero2);
+        break;
+    case 4:
+        console.log("A divisão dos nnúmeros é:", numero1 / numero2);
+        break;
+}
